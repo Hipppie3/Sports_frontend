9dfb221e8602740e97fdbc32d7661652ded82bd9
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2024-06-25 16:02:43 -0700

    Update login form with new heroku route

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -19,7 +19,7 @@ const LoginForm = () => {
         console.log('Sending login request with:', { username, password });
       }
 
-      const response = await axios.post('http://localhost:3000/api/auth/login', { username, password });
+      const response = await axios.post('https://basketballbackend-f112659937b5.herokuapp.com/api/auth/login', { username, password });
 
       if (response.status === 200) {
         console.log('Login successful:', response.data);
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
    server: {
     proxy: {
         '/api': { 
-          target: 'http://localhost:3000/', 
+          target: 'https://basketballbackend-715cf6a06fb1.herokuapp.com/', 
           changeOrigin: true, 
           secure: false, 
           rewrite: (path) => path.replace(/^\/api/, "") 
