1b219a3350e105e2f16839de1d73154c23e80ff6
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2024-09-24 11:02:08 +0700

    testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,7 @@ import './Home.css';
 const Home = () => {
   return (
     <div className="home-container">
-      <h1>HULK</h1>
-      <div className="video-container">
-        <iframe
-          className="responsive-iframe"
-          src="https://www.youtube.com/embed/aldb68CXsAM"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          title="Player of the Week Video"
-        ></iframe>
-      </div>
+     Home Page Coming Soon
     </div>
   );
 };
